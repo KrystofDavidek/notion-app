6811f48ba5dff0af1c8e86c738e46956e40fe1ae
Krystof Davidek <kry.davidek@gmail.com>
2021-06-11 09:09:59 +0200

    Add typescript do backend

---
+++ api/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+let data = [];
+
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(
+  "mongodb+srv://xbetik:notion@cluster0.8ylwm.mongodb.net/notiondb?retryWrites=true&w=majority",
+  (err, client) => {
+    const db = client.db("notiondb");
+
+    db.collection("Page")
+      .find()
+      .toArray(function (err, result) {
+        if (err) throw err;
+
+        console.log(result);
+        data = result;
+        client.close();
+        // db.collection('page').insertOne("Second Page");
+      });
+  }
+);
+
+app.get("/backend", (req, res) => {
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

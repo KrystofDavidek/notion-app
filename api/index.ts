a24949f54c81aa66a77a4054ed356acd10fdb0a7
Andrej Betik <xbetik@fi.muni.cz>
2021-06-11 09:51:21 +0200

    feat: api get-pages, get-pages by id, post added

--- api/index.ts
+++ api/index.ts
@@ -1,33 +1,88 @@
-import express from "express";
-
+const express = require('express');
 const app = express();
 const port = 5000;
-let data = [];
+const uri = "mongodb+srv://xbetik:notion@cluster0.8ylwm.mongodb.net/notiondb?retryWrites=true&w=majority";
+var ObjectId = require('mongodb').ObjectID;
+var MongoClient = require('mongodb').MongoClient
 
-const MongoClient = require("mongodb").MongoClient;
+// Get all pages
+app.get('/get-pages/', function (req, res) {
+    MongoClient.connect(uri, function (err, client) {
+       if (err) throw err
+       var db = client.db('notiondb')
+       db.collection('Page').find().toArray(function (err, result) {
+         if (err) throw err
+         res.send(result)
+       })
+     })
+ })
 
-MongoClient.connect(
-  "mongodb+srv://xbetik:notion@cluster0.8ylwm.mongodb.net/notiondb?retryWrites=true&w=majority",
-  (err, client) => {
-    const db = client.db("notiondb");
+ // Get page by id
+ app.get('/get-page/:id', function (req, res) {
+    MongoClient.connect(uri, function (err, client) {
+       if (err) throw err
+       var db = client.db('notiondb')
+       const result = db.collection('Page').find(ObjectId(req.params.id)).toArray(function (err, result) {
+           if (err) throw err
+           res.send(result);
+       });
+     })
+ })
 
-    db.collection("Page")
-      .find()
-      .toArray(function (err, result) {
-        if (err) throw err;
-
-        console.log(result);
-        data = result;
-        client.close();
-        // db.collection('page').insertOne("Second Page");
-      });
-  }
-);
-
-app.get("/backend", (req, res) => {
-  res.send(data);
-});
+//  Create new page
+ app.post('/create-page', (req, res) => {
+    var newPage = createPage();
+    res.send(insert("Page", newPage));
+ })
+ 
+//  Get users
+app.get('/get-users', (req, res) => {
+    MongoClient.connect(uri, function (err, client) {
+        if (err) throw err
+        var db = client.db('notiondb')
+        const result = db.collection('User').find(ObjectId(req.params.id)).toArray(function (err, result) {
+            if (err) throw err
+            res.send(result);
+        });
+      })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// Insert into collection
+function insert(collectionName, object) {
+    MongoClient.connect(uri, function(err, client) {
+        if (err) throw err
+        var db = client.db('notiondb');
+        db.collection(collectionName).insertOne(object, function(err, newobj) {
+            if (err) throw err
+            console.log("Object was inserted succesfully");
+            return newobj.insertedId;
+        })
+    })
+}
+
+// Creating TEST objects
+
+function createUser() {
+    return {
+        username: "Anonymous",
+        created_at: Date.now(),
+        modified_at: null,
+        deleted_at: null
+    }
+}
+
+function createPage(title="Page", userId=null, iconPath = null) {
+    return {
+        title: title,
+        user_id: userId,
+        modified_info_id: 10,
+        icon_path: iconPath,
+        created_at: Date.now(),
+        modified_at: null,
+        deleted_at: null
+    }
+}
